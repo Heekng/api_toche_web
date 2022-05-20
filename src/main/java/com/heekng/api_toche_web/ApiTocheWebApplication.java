8bf4b9a7b2e45b7f25918b089505f31832ccd8ad
heekng <rhkd1769@gmail.com>
2022-05-20 21:59:34 +0900

    initial commit

---
+++ src/main/java/com/heekng/api_toche_web/ApiTocheWebApplication.java
@@ -0,0 +1,13 @@
+package com.heekng.api_toche_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiTocheWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiTocheWebApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/heekng/api_toche_web/ApiTocheWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heekng.api_toche_web;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiTocheWebApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
