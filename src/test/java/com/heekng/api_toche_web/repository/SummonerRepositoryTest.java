3f4ba807c5744ef79759b1219dd6a2211da89dc9
heekng <rhkd1769@gmail.com>
2022-06-03 22:02:49 +0900

    #29 SummonerRepositoryTest 작성

---
+++ src/test/java/com/heekng/api_toche_web/repository/SummonerRepositoryTest.java
@@ -0,0 +1,56 @@
+package com.heekng.api_toche_web.repository;
+
+import com.heekng.api_toche_web.entity.ChallengerInquiry;
+import com.heekng.api_toche_web.entity.Summoner;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.Optional;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@Transactional
+class SummonerRepositoryTest {
+
+    @PersistenceContext
+    EntityManager em;
+    @Autowired
+    SummonerRepository summonerRepository;
+
+    @Test
+    void basicTest() throws Exception {
+        Summoner summoner = Summoner.builder()
+                .id("summonerId")
+                .name("summonerName")
+                .puuid("123-123-123")
+                .build();
+
+        // save
+        summonerRepository.save(summoner);
+
+        // findById
+        Optional<Summoner> findByIdObject = summonerRepository.findById(summoner.getSummonerId());
+        assertThat(findByIdObject).isNotEmpty();
+        assertThat(findByIdObject.get()).isEqualTo(summoner);
+
+        // findAll
+        List<Summoner> findAllObject = summonerRepository.findAll();
+        assertThat(findAllObject).isNotEmpty();
+        assertThat(findAllObject.size()).isEqualTo(1);
+
+        // delete
+        summonerRepository.delete(summoner);
+        Optional<Summoner> afterDeleteObject = summonerRepository.findById(summoner.getSummonerId());
+        assertThat(afterDeleteObject).isEmpty();
+
+    }
+}
