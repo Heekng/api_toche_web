58070330f5a6a6bcbdbb70eb4fae010b8b611a27
heekng <rhkd1769@gmail.com>
2022-06-04 15:48:14 +0900

    #29 MatchInfoRepositoryTest 작성

--- src/main/java/com/heekng/api_toche_web/repository/MatchInfoRepository.java
+++ src/main/java/com/heekng/api_toche_web/repository/MatchInfoRepository.java
@@ -1,8 +1,9 @@
 package com.heekng.api_toche_web.repository;
 
 import com.heekng.api_toche_web.entity.Match;
+import com.heekng.api_toche_web.entity.MatchInfo;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MatchInfoRepository extends JpaRepository<Match, Long>, MatchInfoRepositoryCustom {
+public interface MatchInfoRepository extends JpaRepository<MatchInfo, Long>, MatchInfoRepositoryCustom {
 
 }
