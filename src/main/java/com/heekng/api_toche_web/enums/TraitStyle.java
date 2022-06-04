d689af6e4336512f517b9da93111cb92d982dcfd
heekng <rhkd1769@gmail.com>
2022-06-04 17:30:26 +0900

    #29 TraitStyle Enum 적용

---
+++ src/main/java/com/heekng/api_toche_web/enums/TraitStyle.java
@@ -0,0 +1,16 @@
+package com.heekng.api_toche_web.enums;
+
+public enum TraitStyle {
+    _0("No style"),
+    _1("Bronze"),
+    _2("Silver"),
+    _3("Gold"),
+    _4("Chromatic");
+
+    private final String styleName;
+
+    TraitStyle(String styleName) {
+        this.styleName = styleName;
+    }
+
+}
