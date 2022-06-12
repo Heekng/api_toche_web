5bda5b1eb6cd0edd52d76f26217224478a4cffff
heekng <rhkd1769@gmail.com>
2022-06-13 01:08:13 +0900

    #37 ApiMatchListJob 스케쥴링 추가

---
+++ src/main/java/com/heekng/api_toche_web/scheduler/ApiMatchListJobRunner.java
@@ -0,0 +1,30 @@
+package com.heekng.api_toche_web.scheduler;
+
+import lombok.RequiredArgsConstructor;
+import org.quartz.JobDetail;
+import org.quartz.Scheduler;
+import org.quartz.SchedulerException;
+import org.quartz.Trigger;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+
+@Component
+@RequiredArgsConstructor
+public class ApiMatchListJobRunner extends JobRunner {
+
+    private final Scheduler scheduler;
+
+    @Override
+    protected void doRun(ApplicationArguments args) {
+        JobDetail jobDetailMaster = buildJobDetail(ApiMatchListSchJob.class, "ApiMathListSchJob", "batch", new HashMap<>());
+        Trigger triggerMaster = buildJobTrigger("0 10 * * * ?");
+
+        try {
+            scheduler.scheduleJob(jobDetailMaster, triggerMaster);
+        } catch (SchedulerException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
