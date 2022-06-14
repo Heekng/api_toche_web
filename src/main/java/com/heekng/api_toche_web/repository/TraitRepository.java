49bde2892b9f9c1a72ed927dc51b1194d7e25186
heekng <rhkd1769@gmail.com>
2022-06-14 14:55:09 +0900

    #45 TraitRepository.findByNameAndSeasonId 작성 및 테스트코드 작성

--- src/main/java/com/heekng/api_toche_web/repository/TraitRepository.java
+++ src/main/java/com/heekng/api_toche_web/repository/TraitRepository.java
@@ -3,6 +3,11 @@ package com.heekng.api_toche_web.repository;
 import com.heekng.api_toche_web.entity.Trait;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface TraitRepository extends JpaRepository<Trait, Long>, TraitRepositoryCustom {
+
     Trait findBySeasonIdAndNameContaining(Long seasonId, String name);
+
+    Optional<Trait> findByNameAndSeasonId(String name, Long seasonId);
 }
