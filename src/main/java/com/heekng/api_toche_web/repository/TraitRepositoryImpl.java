16bad6279f8e1ddc0c730fdb126e83a4b81fd44e
heekng <rhkd1769@gmail.com>
2022-06-16 01:41:53 +0900

    #51 traitRepository.searchByTraitsRequest 작성 및 테스트코드 작성

--- src/main/java/com/heekng/api_toche_web/repository/TraitRepositoryImpl.java
+++ src/main/java/com/heekng/api_toche_web/repository/TraitRepositoryImpl.java
@@ -1,12 +1,47 @@
 package com.heekng.api_toche_web.repository;
 
+import com.heekng.api_toche_web.dto.TraitDTO;
+import com.heekng.api_toche_web.entity.QSeason;
+import com.heekng.api_toche_web.entity.QTrait;
+import com.heekng.api_toche_web.entity.Trait;
+import com.querydsl.core.types.dsl.BooleanExpression;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
+import org.springframework.util.StringUtils;
+
+import java.util.List;
+
+import static com.heekng.api_toche_web.entity.QSeason.*;
+import static com.heekng.api_toche_web.entity.QTrait.*;
+import static org.springframework.util.StringUtils.*;
 
 @RequiredArgsConstructor
 public class TraitRepositoryImpl implements TraitRepositoryCustom {
 
     private final JPAQueryFactory queryFactory;
 
+    @Override
+    public List<Trait> searchByTraitsRequest(TraitDTO.TraitsRequest traitsRequest) {
+        return queryFactory
+                .selectFrom(trait)
+                .leftJoin(trait.season, season)
+                .where(
+                        traitNameContains(traitsRequest.getTraitName()),
+                        seasonIdEq(traitsRequest.getSeasonId())
+                )
+                .orderBy(
+                        season.seasonNum.desc(),
+                        season.seasonName.desc(),
+                        trait.name.asc()
+                )
+                .fetch();
+    }
 
+    private BooleanExpression traitNameContains(String traitName) {
+        return hasText(traitName) ? trait.name.contains(traitName) : null;
+    }
+
+    private BooleanExpression seasonIdEq(Long seasonId) {
+        return seasonId != null ? season.id.eq(seasonId) : null;
+    }
 }
