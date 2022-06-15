9cc47414f267cf885bdc41bb3421ad7a15887962
heekng <rhkd1769@gmail.com>
2022-06-15 17:04:19 +0900

    #51 UnitRepository.findWithSeasonById UnitRepository.searchByUnitsRequest 작성 및 테스트코드 작성

--- src/main/java/com/heekng/api_toche_web/repository/UnitRepositoryCustom.java
+++ src/main/java/com/heekng/api_toche_web/repository/UnitRepositoryCustom.java
@@ -1,4 +1,12 @@
 package com.heekng.api_toche_web.repository;
 
+import com.heekng.api_toche_web.dto.UnitDTO;
+import com.heekng.api_toche_web.entity.Unit;
+
+import java.util.List;
+
 public interface UnitRepositoryCustom {
+
+    List<Unit> searchByUnitsRequest(UnitDTO.UnitsRequest unitsRequest);
+
 }
