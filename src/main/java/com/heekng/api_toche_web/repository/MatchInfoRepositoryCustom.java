4a0b14c31280f964109c7362c4aad7bc1b42f7a7
heekng <rhkd1769@gmail.com>
2022-06-17 17:43:47 +0900

    #61 MatchInfoRepository.searchByUnitContains 작성 및 테스트코드 작성

--- src/main/java/com/heekng/api_toche_web/repository/MatchInfoRepositoryCustom.java
+++ src/main/java/com/heekng/api_toche_web/repository/MatchInfoRepositoryCustom.java
@@ -1,4 +1,10 @@
 package com.heekng.api_toche_web.repository;
 
+import com.heekng.api_toche_web.entity.MatchInfo;
+
+import java.util.List;
+
 public interface MatchInfoRepositoryCustom {
+
+    List<MatchInfo> searchByUnitContains(List<Long> unitIds);
 }
