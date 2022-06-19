3f2686dd519ce070225e30f50df77975c6f3d863
heekng <rhkd1769@gmail.com>
2022-06-20 01:35:35 +0900

    #70 Item entity 필드추가

--- src/main/java/com/heekng/api_toche_web/entity/Item.java
+++ src/main/java/com/heekng/api_toche_web/entity/Item.java
@@ -32,13 +32,40 @@ public class Item extends BaseTimeEntity {
     @Column(name = "name", nullable = false)
     private String name;
 
+    @Column(name = "desc")
+    private String desc;
+    @Column(name = "kor_name")
+    private String korName;
+    @Column(name = "from_item_1")
+    private Integer fromItem1;
+    @Column(name = "from_item_2")
+    private Integer fromItem2;
+    @Column(name = "is_unique")
+    private Boolean isUnique;
+    @Column(name = "icon_path")
+    private String iconPath;
+
     @OneToMany(mappedBy = "item")
     private List<MatchItem> matchItems = new ArrayList<>();
 
     @Builder
-    public Item(Integer num, String name) {
+    public Item(Integer num, String name, String desc, String korName, Integer fromItem1, Integer fromItem2, Boolean isUnique, String iconPath) {
         this.num = num;
         this.name = name;
+        this.desc = desc;
+        this.korName = korName;
+        this.fromItem1 = fromItem1;
+        this.fromItem2 = fromItem2;
+        this.isUnique = isUnique;
+        this.iconPath = iconPath;
     }
 
+    public void updateCDragonData(String desc, String korName, Boolean isUnique, String iconPath, Integer fromItem1, Integer fromItem2) {
+        this.desc = desc;
+        this.korName = korName;
+        this.isUnique = isUnique;
+        this.iconPath = iconPath;
+        this.fromItem1 = fromItem1;
+        this.fromItem2 = fromItem2;
+    }
 }
