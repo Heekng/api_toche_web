46148b725ad61cd415ff003ad5d110f358869e0b
heekng <rhkd1769@gmail.com>
2022-06-21 16:18:34 +0900

    #70 AugmentRepository findByNum, findByNameEndingWith 작성 및 테스트코드 작성

--- src/main/java/com/heekng/api_toche_web/repository/AugmentRepository.java
+++ src/main/java/com/heekng/api_toche_web/repository/AugmentRepository.java
@@ -1,14 +1,15 @@
 package com.heekng.api_toche_web.repository;
 
 import com.heekng.api_toche_web.entity.Augment;
-import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface AugmentRepository extends JpaRepository<Augment, Long>, AugmentRepositoryCustom {
 
     Optional<Augment> findByName(String name);
 
+    Optional<Augment> findByNum(Integer num);
+
+    Optional<Augment> findByNameEndingWith(String name);
 }
