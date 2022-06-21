21a1016133275b94119df6c93c4f965bfe1ffd92
heekng <rhkd1769@gmail.com>
2022-06-21 16:29:21 +0900

    #70 CDragonAllDataPatchJobConfiguration 작성

---
+++ src/main/java/com/heekng/api_toche_web/batch/chunk/processor/CDragonAugmentInsertProcessor.java
@@ -0,0 +1,68 @@
+package com.heekng.api_toche_web.batch.chunk.processor;
+
+import com.heekng.api_toche_web.batch.dto.cDragon.CDragonItemDTO;
+import com.heekng.api_toche_web.entity.Augment;
+import com.heekng.api_toche_web.repository.AugmentRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class CDragonAugmentInsertProcessor implements ItemProcessor<CDragonItemDTO, Augment> {
+
+    private final AugmentRepository augmentRepository;
+
+    @Override
+    public Augment process(CDragonItemDTO cDragonAugmentDTO) throws Exception {
+        // 기존에 추가되어있는 증강체를 지우지 않기 위해 사용
+        String nameEn = cDragonAugmentDTO.getNameEn();
+        if (nameEn == null) {
+            return null;
+        }
+        nameEn = nameEn.replace("'", "");
+        String[] nameEnSplit = nameEn.split(" ");
+        StringBuilder nameEnBuilder = new StringBuilder();
+        for (int i = 0; i < nameEnSplit.length; i++) {
+            if (i != nameEnSplit.length - 1) {
+                nameEnBuilder.append(nameEnSplit[i]);
+            } else {
+                Boolean isAllI = true;
+                String lastStr = nameEnSplit[i];
+                for (char aChar : lastStr.toCharArray()) {
+                    if (aChar != 'I') {
+                        isAllI = false;
+                        break;
+                    }
+                }
+                nameEnBuilder.append(isAllI ? lastStr.length() : lastStr);
+            }
+        }
+        String findNameEn = nameEnBuilder.toString();
+
+        Optional<Augment> augmentOptional = augmentRepository.findByNum(cDragonAugmentDTO.getId());
+        if (augmentOptional.isEmpty()) {
+            augmentOptional = augmentRepository.findByNameEndingWith(findNameEn);
+        }
+        Augment augment = augmentOptional.orElse(
+                Augment.builder()
+                        .name(nameEn)
+                        .build()
+        );
+        Integer num = cDragonAugmentDTO.getId();
+        String desc = cDragonAugmentDTO.getDesc();
+        for (String key : cDragonAugmentDTO.getEffects().keySet()) {
+            desc = desc.replace("@" + key + "@", String.valueOf(cDragonAugmentDTO.getEffects().get(key)));
+        }
+        String korName = cDragonAugmentDTO.getName();
+        Boolean isUnique = cDragonAugmentDTO.getUnique();
+        String iconPath = cDragonAugmentDTO.getIcon().toLowerCase().replace(".dds", ".png");
+        augment.updateCDragonData(num, desc, korName, nameEn, isUnique, iconPath);
+
+        return augment;
+    }
+}
---
+++ src/main/java/com/heekng/api_toche_web/batch/chunk/processor/CDragonItemInsertProcessor.java
@@ -0,0 +1,49 @@
+package com.heekng.api_toche_web.batch.chunk.processor;
+
+import com.heekng.api_toche_web.batch.dto.cDragon.CDragonItemDTO;
+import com.heekng.api_toche_web.entity.Item;
+import com.heekng.api_toche_web.repository.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.batch.item.ItemProcessor;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class CDragonItemInsertProcessor implements ItemProcessor<CDragonItemDTO, Item> {
+
+    private final ItemRepository itemRepository;
+
+    @Override
+    public Item process(CDragonItemDTO cDragonItemDTO) throws Exception {
+
+        Optional<Item> itemOptional = itemRepository.findByNum(cDragonItemDTO.getId());
+        Item item = itemOptional.orElse(
+                Item.builder()
+                        .num(cDragonItemDTO.getId())
+                        .name(cDragonItemDTO.getName())
+                        .build()
+        );
+
+        String desc = cDragonItemDTO.getDesc();
+        String korName = cDragonItemDTO.getName();
+        Boolean isUnique = cDragonItemDTO.getUnique();
+        String iconPath = cDragonItemDTO.getIcon().toLowerCase().replace(".dds", ".png");
+        Integer fromItem1 = null;
+        Integer fromItem2 = null;
+        if (!cDragonItemDTO.getFrom().isEmpty()) {
+            fromItem1 = cDragonItemDTO.getFrom().get(0);
+            fromItem2 = cDragonItemDTO.getFrom().get(1);
+        }
+        for (String key : cDragonItemDTO.getEffects().keySet()) {
+            desc = desc.replace("@" + key + "@", String.valueOf(cDragonItemDTO.getEffects().get(key)));
+        }
+        log.info("desc: {}", desc);
+        item.updateCDragonData(desc, korName, isUnique, iconPath, fromItem1, fromItem2);
+
+        return item;
+    }
+}
