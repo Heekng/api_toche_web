06d55ddf8ebcd7cdfbf7ceddef8a33ccd29810c1
heekng <rhkd1769@gmail.com>
2022-06-22 21:13:32 +0900

    #76 ItemService.itemByItemId 작성

--- src/main/java/com/heekng/api_toche_web/api/ItemApiController.java
+++ src/main/java/com/heekng/api_toche_web/api/ItemApiController.java
@@ -37,8 +37,6 @@ public class ItemApiController {
     public ItemDTO.ItemDetailResponse itemByItemId(
             @PathVariable(name = "itemId", required = true) Long itemId
     ) {
-        Item item = itemRepository.findById(itemId)
-                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 Item 입니다."));
-        return standardMapper.map(item, ItemDTO.ItemDetailResponse.class);
+        return itemService.findItemDetail(itemId);
     }
 }
--- src/main/java/com/heekng/api_toche_web/service/ItemService.java
+++ src/main/java/com/heekng/api_toche_web/service/ItemService.java
@@ -4,7 +4,10 @@ import com.heekng.api_toche_web.dto.ItemDTO;
 import com.heekng.api_toche_web.entity.Item;
 import com.heekng.api_toche_web.entity.Season;
 import com.heekng.api_toche_web.repository.ItemRepository;
+import com.heekng.api_toche_web.repository.MatchItemRepository;
+import com.heekng.api_toche_web.repository.UnitRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -14,9 +17,12 @@ import java.util.Optional;
 @Service
 @Transactional(readOnly = true)
 @RequiredArgsConstructor
+@Slf4j
 public class ItemService {
 
     private final ItemRepository itemRepository;
+    private final MatchItemRepository matchItemRepository;
+    private final UnitRepository unitRepository;
 
     @Transactional
     public Item findOrSave(String name, Integer num, Season season) {
@@ -36,5 +42,15 @@ public class ItemService {
 
     public List<Item> findByItemsRequest(ItemDTO.ItemsRequest itemsRequest) {
         return itemsRequest.getSeasonId() == null ? itemRepository.searchByItemsRequest(itemsRequest) : itemRepository.searchByItemsRequestContainsSeasonId(itemsRequest);
+    }
+
+    public ItemDTO.ItemDetailResponse findItemDetail(Long itemId) {
+        ItemDTO.ItemDetailResponse itemDetailResponse = itemRepository.searchWithFromItemByItemId(itemId)
+                .orElseThrow(() -> new IllegalArgumentException("존재하지 않는 Item 입니다."));
+        Long itemUsedCount = matchItemRepository.countByItemId(itemId);
+        itemDetailResponse.setUsedCount(itemUsedCount);
+//        List<UnitDTO.ItemRankResponse> itemRankResponses = unitRepository.searchUnitRankByItemId(itemId);
+//        itemDetailResponse.getUsedRankUnits().addAll(itemRankResponses);
+        return itemDetailResponse;
     }
 }
