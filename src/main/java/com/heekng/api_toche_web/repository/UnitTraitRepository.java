9ffd22ba1be1d78fa6f5a0efed6dc854cef9d583
heekng <rhkd1769@gmail.com>
2022-06-23 17:09:59 +0900

    #76 UnitTraitRepository.findByUnitIdAndTraitId 작성 및 테스트코드 작성

--- src/main/java/com/heekng/api_toche_web/repository/UnitTraitRepository.java
+++ src/main/java/com/heekng/api_toche_web/repository/UnitTraitRepository.java
@@ -3,6 +3,9 @@ package com.heekng.api_toche_web.repository;
 import com.heekng.api_toche_web.entity.UnitTrait;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface UnitTraitRepository extends JpaRepository<UnitTrait, Long>, UnitTraitRepositoryCustom {
 
+    Optional<UnitTrait> findByUnitIdAndTraitId(Long unitId, Long traitId);
 }
