a202ec3082d3f434069c3e1876f0f5ec02197dbb
heekng <rhkd1769@gmail.com>
2022-07-01 21:30:46 +0900

    #118 useDeckUnitAugmentRepository.findByUseDeckAugment 작성 및 테스트코드 작성

--- src/main/java/com/heekng/api_toche_web/repository/UseDeckUnitAugmentRepository.java
+++ src/main/java/com/heekng/api_toche_web/repository/UseDeckUnitAugmentRepository.java
@@ -5,9 +5,12 @@ import com.heekng.api_toche_web.entity.UseDeckUnit;
 import com.heekng.api_toche_web.entity.UseDeckUnitAugment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UseDeckUnitAugmentRepository extends JpaRepository<UseDeckUnitAugment, Long>, UseDeckUnitAugmentRepositoryCustom {
 
     Optional<UseDeckUnitAugment> findByUseDeckUnitAndUseDeckAugment(UseDeckUnit useDeckUnit, UseDeckAugment useDeckAugment);
+
+    List<UseDeckUnitAugment> findByUseDeckAugment(UseDeckAugment useDeckAugment);
 }
