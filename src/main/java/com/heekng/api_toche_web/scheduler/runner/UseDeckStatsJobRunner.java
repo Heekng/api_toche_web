63741293047568c48e0fa27214585c485ffe6b6f
heekng <rhkd1769@gmail.com>
2022-07-01 20:10:16 +0900

    #118 UseDeckStatsJob 스케쥴링 추가

---
+++ src/main/java/com/heekng/api_toche_web/scheduler/runner/UseDeckStatsJobRunner.java
@@ -0,0 +1,32 @@
+package com.heekng.api_toche_web.scheduler.runner;
+
+import com.heekng.api_toche_web.scheduler.schedule.SeasonMappingItemAndAugmentSchJob;
+import com.heekng.api_toche_web.scheduler.schedule.UseDeckStatsSchJob;
+import lombok.RequiredArgsConstructor;
+import org.quartz.JobDetail;
+import org.quartz.Scheduler;
+import org.quartz.SchedulerException;
+import org.quartz.Trigger;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.stereotype.Component;
+
+import java.util.HashMap;
+
+@Component
+@RequiredArgsConstructor
+public class UseDeckStatsJobRunner extends JobRunner {
+
+    private final Scheduler scheduler;
+
+    @Override
+    protected void doRun(ApplicationArguments args) {
+        JobDetail jobDetailMaster = buildJobDetail(UseDeckStatsSchJob.class, "UseDeckStatsSchJob", "batch", new HashMap<>());
+        Trigger triggerMaster = buildJobTrigger("0 5 1/2 * * ?");
+
+        try {
+            scheduler.scheduleJob(jobDetailMaster, triggerMaster);
+        } catch (SchedulerException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
