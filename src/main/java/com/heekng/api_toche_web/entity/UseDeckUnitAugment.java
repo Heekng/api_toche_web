9656546adc44ad8ea379367436fa8d9212a9cfad
heekng <rhkd1769@gmail.com>
2022-07-01 17:52:42 +0900

    #118 UseDeckUnitAugment Entity 추가

--- src/main/java/com/heekng/api_toche_web/entity/UseDeckUnitAugment.java
+++ src/main/java/com/heekng/api_toche_web/entity/UseDeckUnitAugment.java
@@ -7,34 +7,31 @@ import lombok.NoArgsConstructor;
 
 import javax.persistence.*;
 
-import java.util.ArrayList;
-import java.util.List;
-
-import static javax.persistence.CascadeType.ALL;
 import static javax.persistence.FetchType.LAZY;
 
 @Entity
 @Getter
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
-public class UseDeck {
+public class UseDeckUnitAugment {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    @Column(name = "use_deck_id")
+    @Column(name = "use_deck_unit_augment_id")
     private Long id;
     @Column(name = "use_count")
     private Long useCount;
 
     @ManyToOne(fetch = LAZY)
+    @JoinColumn(name = "use_deck_unit_id", nullable = false)
+    private UseDeckUnit useDeckUnit;
+    @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "use_deck_augment_id", nullable = false)
     private UseDeckAugment useDeckAugment;
 
-    @OneToMany(mappedBy = "useDeck", cascade = ALL)
-    private List<UseDeckUnit> useDeckUnits = new ArrayList<>();
-
     @Builder
-    public UseDeck(Long useCount, UseDeckAugment useDeckAugment) {
+    public UseDeckUnitAugment(Long useCount, UseDeckUnit useDeckUnit, UseDeckAugment useDeckAugment) {
         this.useCount = useCount;
+        this.useDeckUnit = useDeckUnit;
         this.useDeckAugment = useDeckAugment;
     }
 }
